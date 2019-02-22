c231c05ca80f312d100d2cf2e9531ac0b3423c14
Vinicius Zorzanelli <vinizorza@hotmail.com>
2019-02-22 14:13:25 -0300

    Criação arquitetura backend

---
+++ src/main/java/com/ligapaycorp/ligapaymanager/LigapaymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.ligapaycorp.ligapaymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigapaymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigapaymanagerApplication.class, args);
+	}
+
+}
