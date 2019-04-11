7524a52ba34d9e1330ddd4b3147b16ce751e842e
savioserra <serra.santos.s@gmail.com>
2019-04-11 18:41:48 -0300

    Improvement of deploy script

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,5 +1,10 @@
+# Install Heroku Cli
 curl https://cli-assets.heroku.com/install.sh | sh
-docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
-docker tag ligapay:latest registry.heroku.com/$APP_NAME/$HEROKU_PROCESS
-docker push registry.heroku.com/$APP_NAME/$HEROKU_PROCESS
+# Docker Login
+heroku auth:token | docker login --username=_ --password-stdin registry.heroku.com
+# Tag Image
+docker tag ligapay:latest registry.heroku.com/ligapay/web
+# Push to heroku repository
+docker push registry.heroku.com/ligapay/web
+# Release
 heroku container:release web -a ligapay
